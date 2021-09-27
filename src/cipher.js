e1cd01c63f8f0193281c6fdc02fca28d379aedbd
Elizabeth Macias <elizabeth130394@gmail.com>
2021-09-27 10:49:40 -0300

    cambios finales

--- src/cipher.js
+++ src/cipher.js
@@ -1,12 +1,16 @@
 const cipher = {
+  //metodo encode
   encode: (offset,message) => { 
     let mensajeCifrado = "";
-    
+    // indicamos que se produsca un error si el usuario no ingresa un offset tipo number o de valor "0"
     if (offset === null || offset === 0){
       throw new TypeError;
     }
+    //declaramos el ciclo for para que itere caracter por caracter
     for (let i = 0; i < message.length; i++) {
+      //obtenemos el la posición que ocupa el caracter en el codigo ascii
       let codigoAscii = message.charCodeAt(i);
+      //mediante if else indicamos que cifre letras mayusculas, minusculas y que los caracteres que no cumplen esta condición se devuelva ese mismo caracter
       if (codigoAscii >= 65 && codigoAscii <= 90){
         let nuevoCodigoAsciiMayusculas = ((codigoAscii - 65 + parseInt(offset)) % 26) + 65;
         mensajeCifrado += String.fromCharCode(nuevoCodigoAsciiMayusculas);
@@ -18,15 +22,19 @@ const cipher = {
       }
     }
     return mensajeCifrado;
-  },   
+  },  
+  //metodo encode  
   decode: (offset,message) => { 
     let mensajeDescifrado = "";
-    
+    // indicamos que se produsca un error si el usuario no ingresa un offset tipo number o de valor "0"
     if (offset === null || offset === 0){
       throw new TypeError;
     }
+    //declaramos el ciclo for para que itere caracter por caracter
     for (let i = 0; i < message.length; i++) {
+      //obtenemos el la posición que ocupa el caracter en el codigo ascii
       let codigoAscii = message.charCodeAt(i);
+      //mediante if else indicamos que descifre letras mayusculas, minusculas y que los caracteres que no cumplen esta condición se devuelva ese mismo caracter
       if (codigoAscii >= 65 && codigoAscii <= 90){
         let nuevoCodigoAsciiMayusculas = ((codigoAscii - 90 - parseInt(offset)) % 26) + 90;
         mensajeDescifrado += String.fromCharCode(nuevoCodigoAsciiMayusculas);
--- src/index.js
+++ src/index.js
@@ -1,27 +1,30 @@
 import cipher from './cipher.js';
 //declaramos la funcion funcionCifrar
 const funcionCifrar = () =>{
-  let desplazamiento = document.getElementById("desplazamiento").value; //con let desplazamiento obtendremos el dato que nos entrega el input de id desplazamiento (el cual es llamado usando getElementById)
-  let mensaje = document.getElementById("mensaje").value; //con let mensaje obtendremos el dato que nos entrega el input de id mensaje(el cual es llamado usando getElementById)
-  document.getElementById("mensaje2").value = cipher.encode(desplazamiento,mensaje); //usando getElementById llamo al input mensaje2 en el cual se imprimira el mensaje ya cifado indicando que este es igual al objeto cipher.encode
+  let desplazamiento = document.getElementById("desplazamiento").value;
+  let mensaje = document.getElementById("mensaje").value; 
+  document.getElementById("mensaje2").value = cipher.encode(desplazamiento,mensaje); 
 }
-const btCifrar = document.getElementById("btCifrar"); //declaramos la variable btCifrar la cual mediante getElementById le indicamos el id btCifrar que corresponde al boton cifrar que ejecuta la funcionCifrar 
-btCifrar.addEventListener("click", funcionCifrar); //a btCifrar le indicamos que cuando se produsca el evento click se ejecute la funcionCifrar
+//declaramos la variable btCifrar
+const btCifrar = document.getElementById("btCifrar"); 
+btCifrar.addEventListener("click", funcionCifrar);
 //declaramos la funcion funcionDescifrar
 const funcionDescifrar = () =>{
-  let desplazamiento = document.getElementById("desplazamiento").value; //con let desplazamiento obtendremos el dato que nos entrega el input de id desplazamiento (el cual es llamado usando getElementById)
-  let mensaje = document.getElementById("mensaje").value; //con let mensaje obtendremos el dato que nos entrega el input de id mensaje(el cual es llamado usando getElementById)
-  document.getElementById("mensaje2").value = cipher.decode(desplazamiento,mensaje); //usando getElementById llamo al input mensaje2 en el cual se imprimira el mensaje ya decifado indicando que este es igual al objeto cipher.decode
+  let desplazamiento = document.getElementById("desplazamiento").value; 
+  let mensaje = document.getElementById("mensaje").value; 
+  document.getElementById("mensaje2").value = cipher.decode(desplazamiento,mensaje); 
 }
-const btDescifrar = document.getElementById("btDescifrar"); //declaramos la variable btDecifrar la cual mediante getElementById le indicamos el id btDescifrar que corresponde al boton descifrar que ejecuta la funcionDescifrar 
-btDescifrar.addEventListener("click", funcionDescifrar); //a btDescifrar le indicamos que cuando se produsca el evento click se ejecute la funcionDescifrar
-//declaramos la funcion limpiar
+//declaramos la variable btDecifrar
+const btDescifrar = document.getElementById("btDescifrar"); 
+btDescifrar.addEventListener("click", funcionDescifrar);
+//declaramos la funcion funcionLimpiar
 const funcionLimpiar = () =>{
   document.getElementById("mensaje").value = "";
   document.getElementById("mensaje2").value ="";
   document.getElementById("desplazamiento").value = 0;
 }
-const btLimpiar = document.getElementById("btLimpiar"); //declaramos la variable btLimpiar 
+//declaramos la variable btLimpiar
+const btLimpiar = document.getElementById("btLimpiar");
 btLimpiar.addEventListener("click", funcionLimpiar);
 
 
